03fa6471b214593dc1b244d46a219e223217cf21
piotr <piotrek.priv.michalski@wp.pl>
2023-04-09 13:56:42 +0200

    add tables with body content

--- src/pages/body/index.tsx
+++ src/pages/body/index.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import ContentLayout from "~/components/ContentLayout";
 import { useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
-import { GoArrowDown } from "react-icons/all";
 import { GoChevronDown } from "react-icons/go";
-import { allowedDisplayValues } from "next/dist/compiled/@next/font/dist/constants";
-import TrainingTable from "~/components/TrainingTable";
-import { type Weight } from ".prisma/client";
+import { type Kcal, type Weight, type Body } from ".prisma/client";
 
 function Index() {
   const { user } = useUser();
@@ -15,26 +12,57 @@ function Index() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
   if (!data) return <div>No data</div>;
-  const config = [];
+
+  const kcalConfig = [
+    {
+      label: "Data",
+      render: (kcal: Kcal) => (
+        <span>{kcal.createdAt.toLocaleDateString()}</span>
+      ),
+    },
+    {
+      label: "Pomiary",
+      render: (kcal: Kcal) => <span>{`${kcal.kcal} kcal`}</span>,
+    },
+  ];
+
+  const weightConfig = [
+    {
+      label: "Data",
+      render: (weight: Weight) => (
+        <span>{weight.createdAt.toLocaleDateString()}</span>
+      ),
+    },
+    {
+      label: "Pomiary",
+      render: (weight: Weight) => <span>{`${weight.weight} kg`}</span>,
+    },
+  ];
+  const bodyConfig = [
+    {
+      label: "Data",
+      render: (body: Body) => (
+        <span>{body.createdAt.toLocaleDateString()}</span>
+      ),
+    },
+    {
+      label: "Pomiary",
+      render: () => <GoChevronDown />,
+    },
+  ];
+
   return (
     <ContentLayout>
       <h1 className="my-10 text-3xl text-cyan-400">{user?.fullName}</h1>
-      <div className="flex flex-col justify-center gap-10 md:flex-row ">
-        <BodyStats config={} data={data.weight} />
-        <div>
-          <h2>Body measurements</h2>
-          <div className="flex items-center justify-between">
-            {data.body.map((body) => (
-              <div key={body.id}>
-                <span>{body.createdAt.toLocaleDateString()}</span>
-              </div>
-            ))}
-            <GoChevronDown className="h-6" />
-          </div>
-        </div>{" "}
-        <div>
-          <h2>Kcal</h2>
-          <div>content</div>
+      <div className="flex">
+        <div className="container mx-auto grid max-w-3xl grid-cols-[1fr_1fr] gap-4 px-2 py-6 text-center text-sm md:text-lg">
+          <BodyStats config={weightConfig} data={data.weight} title="Weight" />
+        </div>
+        <div className="container mx-auto grid max-w-3xl grid-cols-[1fr_1fr] gap-4 px-2 py-6 text-center text-sm md:text-lg">
+          <BodyStats config={kcalConfig} data={data.kcal} title="Kcal" />
+        </div>
+        <div className="container mx-auto grid max-w-3xl grid-cols-[1fr_1fr] gap-4 px-2 py-6 text-center text-sm md:text-lg">
+          <BodyStats config={bodyConfig} data={data.body} title="Body" />
         </div>
       </div>
     </ContentLayout>
@@ -42,14 +70,19 @@ function Index() {
 }
 export default Index;
 //@TODO ogarnac to, zrobic cos na wzor trainingtable tylko ze dla bodystats
-interface BodyStatsProps {
+interface BodyStatsProps<T> {
   config: {
     label: string;
-    render: (weight: Weight[], index: number) => React.ReactNode;
-  };
-  data: Weight[];
+    render: (data: T) => JSX.Element;
+  }[];
+  data: T[];
+  title: string;
 }
-export function BodyStats({ config, data }: BodyStatsProps) {
+export function BodyStats<T extends { id: string }>({
+  config,
+  data,
+  title,
+}: BodyStatsProps<T>) {
   const renderedLabels = config.map((column) => {
     return (
       <h2 className="" key={column.label}>
@@ -58,11 +91,11 @@ export function BodyStats({ config, data }: BodyStatsProps) {
     );
   });
 
-  const readOnlyRows = data?.map((rowData, index) => {
+  const readOnlyRows = data?.map((rowData) => {
     const renderedCells = config.map((column) => {
       return (
         <div key={column.label} className="flex justify-center ">
-          {column.render(rowData, index)}
+          {column.render(rowData)}
         </div>
       );
     });
@@ -75,6 +108,7 @@ export function BodyStats({ config, data }: BodyStatsProps) {
 
   return (
     <>
+      <h2 className="col-span-2">{title}</h2>
       <div className="contents">{renderedLabels}</div>
       {readOnlyRows}
     </>
